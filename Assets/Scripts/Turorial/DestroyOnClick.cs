29b5b718794a9e2d38c668a396ef7d875799c0bd
Alex Chaplygin <chapalexander@gmail.com>
2023-07-17 18:52:28 +0300

    Game Tutorial Added

---
+++ Assets/Scripts/Turorial/DestroyOnClick.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnClick : MonoBehaviour
+{
+	[SerializeField]
+	private InputReaderSO input;
+	
+	private void Start()
+	{
+		input.TouchEvent += DestroySelf;
+	}
+
+	private void DestroySelf()
+	{
+		input.TouchEvent -= DestroySelf;
+		Destroy(gameObject);
+	}
+}
